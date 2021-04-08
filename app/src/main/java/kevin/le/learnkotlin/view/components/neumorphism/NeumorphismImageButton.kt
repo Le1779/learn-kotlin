4f4e24197ce17ba900616dda68eafce6a47f5bcd
kevinle <le8419951779@gmail.com>
2021-04-08 15:20:47 +0800

    [Add] NeumorphismImageButton

---
+++ app/src/main/java/kevin/le/learnkotlin/view/components/neumorphism/NeumorphismImageButton.kt
@@ -0,0 +1,30 @@
+package kevin.le.learnkotlin.view.components.neumorphism
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.MotionEvent
+import androidx.appcompat.widget.AppCompatImageButton
+import kevin.le.learnkotlin.view.components.neumorphism.shadow.Corner
+import kevin.le.learnkotlin.view.components.neumorphism.shadow.State
+
+class NeumorphismImageButton(
+        context: Context,
+        attrs: AttributeSet? = null
+): AppCompatImageButton(context, attrs) {
+    private val drawable: NeumorphismDrawable = NeumorphismDrawable(context)
+
+    init {
+        drawable.corner = Corner.CIRCLE
+        setBackgroundDrawable(drawable)
+    }
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN ->
+                drawable.state = State.DOWN
+            MotionEvent.ACTION_UP ->
+                drawable.state = State.UP
+        }
+        return super.onTouchEvent(event)
+    }
+}
