a7853e5b01331cbcd125831ec20c8d407864587b
kevinle <le8419951779@gmail.com>
2021-04-22 15:33:49 +0800

    [Add] NeumorphismImageButton attrs

--- app/src/main/java/kevin/le/learnkotlin/view/components/neumorphism/NeumorphismImageButton.kt
+++ app/src/main/java/kevin/le/learnkotlin/view/components/neumorphism/NeumorphismImageButton.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.MotionEvent
 import androidx.appcompat.widget.AppCompatImageButton
+import kevin.le.learnkotlin.R
 import kevin.le.learnkotlin.view.components.neumorphism.shadow.Corner
 import kevin.le.learnkotlin.view.components.neumorphism.shadow.State
 
@@ -11,10 +12,24 @@ class NeumorphismImageButton(
         context: Context,
         attrs: AttributeSet? = null
 ): AppCompatImageButton(context, attrs) {
-    val drawable: NeumorphismDrawable = NeumorphismDrawable(context)
+    val drawable: NeumorphismDrawable
 
     init {
-        drawable.corner = Corner.CIRCLE
+        val a = context.obtainStyledAttributes(
+            attrs, R.styleable.NeumorphismImageButton
+        )
+
+        val corner = a.getInt(R.styleable.NeumorphismImageButton_neumorphism_corner, 1)
+        var cornerRadius = a.getDimension(R.styleable.NeumorphismImageButton_neumorphism_corner_radius, -1f)
+
+        drawable = NeumorphismDrawable(context).apply {
+            this.corner = Corner.values()[corner]
+            if (cornerRadius == -1f) {
+                cornerRadius = this.corner.radius
+            }
+
+            this.radius = cornerRadius
+        }
         background = drawable
     }
 
